48e4c7a18b04a8780e103b22038c837d1f0fc203
tgu85 <Theresia.Gunkel@rub.de>
2021-01-15 20:05:59 +0100

    first real commit

---
+++ src/Titel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Titel.css';
+
+class Titel extends Component {
+	render () {
+		return (
+			<h1 className="headTitel"> Random Weekly Meal Schedule</h1>);
+	}
+}
+
+export default Titel;
