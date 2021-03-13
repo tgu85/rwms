4283d9d899504fcceb056285da20b564edb5c607
tgu85 <Theresia.Gunkel@rub.de>
2021-03-13 21:31:23 +0100

    adding credentials to the fetch functions

--- src/login.js
+++ src/login.js
@@ -22,7 +22,8 @@ class Login extends Component {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(user)
+                body: JSON.stringify(user),
+                credentials: "include",
             }
         )
         if (response.ok) {
