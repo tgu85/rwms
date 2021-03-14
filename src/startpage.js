045b96bbadca4829f45ef49ca85178c4a6b88bf2
tgu85 <Theresia.Gunkel@rub.de>
2021-03-14 09:01:42 +0100

    added styles to the startpage and tried a new linking option

--- src/startpage.js
+++ src/startpage.js
@@ -20,37 +20,38 @@ class Startpage extends Component {
                         </header>
                     </Row>
                 <Row>
-                    <Col>
-                        <p className="title">Create</p>
-                        <div className="startText">
+                    <Col className="startText">
+                        <p className="titleStart">Create</p>
+                        <div>
                             <p>You already have a bunch of recipes?
                                 Add them to your own recipe database and create your random meal plan with your own recipes.</p>
                             <p>So you know exactly what to do when it comes to cooking and you do not have to learn new, maybe difficult recipes. </p>
                             <p>Let's start and create your own digital cookbook!</p>
                         </div>
                         <button onClick={
-                            <Link to="/addingredients"/>
+                            //<Link to="/addingredients"/>
+                            <a href="https://kraut-und-rueben.herokuapp.com/addingredients"></a>
                         }>Go!
                         </button>
                     </Col>
 
-                    <Col>
-                        <p className="title">Enjoy</p>
-                        <div className="startText">
+                    <Col className="startText">
+                        <p className="titleStart">Enjoy</p>
+                        <div className="startBox">
                             <p>In the Enjoy section you can let the website create a random weekly meal plan for you.</p>
                             <p>If you have added any own recipes in the Create section, the random meal plan will be filled with your own recipes.
                             Letting the website tell you what to cook on each day of a week saves you a lot of time.
                                 Besides that, you can go grocery shopping at the beginning of the week and thus reduce the amount of going grocery shopping.</p>
                             <p>The random meal plan can help you to force a healthy lifestyle, because you can easily plan your meals for the week and avoid ordering too many pizzas a week. </p>
                         </div>
-                        <button>Go!
+                        <button className="butStart">Go!
                             <Link to={location => ({ ...location, pathname: "/recipess" })} />
                         </button>
                     </Col>
 
-                    <Col>
-                        <p className="title">Discover</p>
-                        <div className="startText">
+                    <Col className="startText">
+                        <p className="titleStart">Discover</p>
+                        <div>
                             <p>This is your digital cookbook.</p>
                             <p>Here you get an overview over your own recipe database.</p>
                             <p>Every added recipe will be shown in this section, so you will always remember what amazing recipes you know and love.</p>
