5be22d1c1c6e250efd74938fe83a0b9cc290f2f5
tgu85 <Theresia.Gunkel@rub.de>
2021-03-14 16:41:07 +0100

    style changes on the startpage

--- src/startpage.js
+++ src/startpage.js
@@ -10,12 +10,12 @@ class Startpage extends Component {
     render () {
         return (
             <div>
+                <div id="headerpic">
+                <header>
+                    <img src={headerpic}></img>
+                </header>
+                </div>
                 <Container>
-                    <Row id="headerRow">
-                        <header>
-                            <img src={headerpic} id="headerpic"></img>
-                        </header>
-                    </Row>
                 <Row>
                     <Col className="startText">
                         <p className="titleStart">Create</p>
@@ -25,25 +25,27 @@ class Startpage extends Component {
                             <p>So you know exactly what to do when it comes to cooking and you do not have to learn new, maybe difficult recipes. </p>
                             <p>Let's start and create your own digital cookbook!</p>
                         </div>
-                        <Link className="linkStart" to="/addingredients">
+                        <div className="butStart">
+                        <Link to="/addingredients">
                         <button>Go!</button>
                         </Link>
+                        </div>
                     </Col>
 
                     <Col className="startText">
                         <p className="titleStart">Enjoy</p>
                         <div className="startBox">
-                            <div>
                             <p>In the Enjoy section you can let the website create a random weekly meal plan for you.</p>
                             <p>If you have added any own recipes in the Create section, the random meal plan will be filled with your own recipes.
                             Letting the website tell you what to cook on each day of a week saves you a lot of time.
                                 Besides that, you can go grocery shopping at the beginning of the week and thus reduce the amount of going grocery shopping.</p>
                             <p>The random meal plan can help you to force a healthy lifestyle, because you can easily plan your meals for the week and avoid ordering too many pizzas a week. </p>
                             </div>
-                            </div>
-                        <Link className="linkStart" to="/randommealplan">
-                            <button className="butStart">Go!</button>
+                        <div className="butStart">
+                        <Link to="/randommealplan">
+                            <button>Go!</button>
                         </Link>
+                        </div>
                     </Col>
 
                     <Col className="startText">
@@ -53,7 +55,9 @@ class Startpage extends Component {
                             <p>Here you get an overview over your own recipe database.</p>
                             <p>Every added recipe will be shown in this section, so you will always remember what amazing recipes you know and love.</p>
                         </div>
+                        <div className="butStart">
                         <button>Go!</button>
+                        </div>
                     </Col>
                 </Row>
                 </Container>
